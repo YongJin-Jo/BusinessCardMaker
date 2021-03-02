68d8adc13eb6f6820dd72956960d4b7707bbc2d9
YongJin-Jo <artinc5233@gmail.com>
2021-03-02 21:16:41 +0900

    project set up & add firebase

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,24 @@
-import './app.css';
+import './app.module.css';
+import Login from './component/login';
 
-function App() {
-  return <h1>Hellow</h1>
+function App({firebase}) {
+  firebase.initializeApp(firebase.firebaseConfig);
+  firebase.analytics();
+  return(
+    <>
+      <header>
+        <img src="../images/logo.png" alt=""/>
+        <h1>Business Card Maker</h1>
+      </header>
+      <main>
+       <Login/>
+
+      </main>
+      <footer>
+        <span>Code your dream</span>
+      </footer>
+    </>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.module.css';
 import App from './app';
+import FireBase from './service/firebase'
+
+const firebase = new FireBase()
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App 
+    firebase = {firebase}
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
